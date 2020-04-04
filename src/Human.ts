ac1192a77e2674d4f97445656f0e2fd44688bb42
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-04-04 13:16:29 +0530

    Added Human class

---
+++ src/Human.ts
@@ -0,0 +1,32 @@
+
+// Imports
+import p5 from 'p5'
+
+// Interface for Human class
+export interface IHuman {
+	position: p5.Vector
+	draw(sketch: p5): void
+}
+
+// Human class
+export class Human implements IHuman {
+
+	public position: p5.Vector
+
+	/**
+	 * Constructs a Human instance
+	 * @param pos The position of the human
+	 */
+	constructor(pos: p5.Vector) {
+		this.position = pos
+	}
+
+	/**
+	 * Draws the Human
+	 * @param sketch The sketch to draw on
+	 */
+	public draw(sketch: p5): void {
+		//
+	}
+
+}
