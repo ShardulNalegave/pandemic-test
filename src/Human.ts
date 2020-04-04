4f71268ef34bcc8176eea24018f7f92b3139b952
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-04-04 14:15:01 +0530

    Experimenting with human movement

--- src/Human.ts
+++ src/Human.ts
@@ -5,13 +5,16 @@ import p5 from 'p5'
 // Interface for Human class
 export interface IHuman {
 	position: p5.Vector
+	velocity: p5.Vector | null
 	draw(sketch: p5): void
+	move(sketch: p5): void
 }
 
 // Human class
 export class Human implements IHuman {
 
 	public position: p5.Vector
+	public velocity: p5.Vector | null = null
 
 	/**
 	 * Constructs a Human instance
@@ -26,9 +29,22 @@ export class Human implements IHuman {
 	 * @param sketch The sketch to draw on
 	 */
 	public draw(sketch: p5): void {
+		this.move(sketch)
 		sketch.stroke(0)
 		sketch.fill(255)
 		sketch.ellipse(this.position.x, this.position.y, 10, 10)
+	}
+
+	/**
+	 * Move the human
+	 */
+	public move(sketch: p5): void {
+		let to: p5.Vector = sketch.createVector(
+			sketch.random(0, 600),
+			sketch.random(0, 600)
+		)
+		this.velocity = p5.Vector.sub(to, this.position).normalize()
+		this.position.add(this.velocity)
 	}
 
 }
